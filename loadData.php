9e751256fb1b018f6f5e74e7c2a29ddb3a2de2a6
lattes <liaojiafeng93@gmail.com>
2017-03-02 02:14:21 -0600

    start search book

--- loadData.php
+++ loadData.php
@@ -1,7 +1,7 @@
 <?php
 	$mysql = mysqli_connect("localhost:8889","root","root","Library");
 	if (mysqli_connect_errno()){
-		echo "Failed to connect to MySQL: ".mysqli_connect_error()."\n";
+		echo "Failed to connect to MySQL: ".mysqli_connect_error()."<br/>";
 	}
 	//////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////
@@ -15,7 +15,7 @@
 
 		$insert_book = "INSERT INTO BOOK VALUES ('$isbn','$title');";
 		if (!mysqli_query($mysql,$insert_book)) {
-			echo "Error: ".$insert_book.mysqli_error($mysql)."\n";
+			echo "Error: ".$insert_book.mysqli_error($mysql)."<br/>";
 		}
 
 		if (strpos($authors,'&amp;') !== false) {
@@ -35,22 +35,22 @@
 				$row = mysqli_fetch_array($result);
 				$insert_book_authors = "INSERT IGNORE INTO BOOK_AUTHORS VALUES ($row[0],'$isbn');";
 				if (!mysqli_query($mysql,$insert_book_authors)) {
-					echo "Error: ".$insert_book_authors.mysqli_error($mysql)."\n";
+					echo "Error: ".$insert_book_authors.mysqli_error($mysql)."<br/>";
 				}	
 			}else{
 				$insert_author = "INSERT INTO AUTHORS VALUES ('$author_id','$author');";
 				if (!mysqli_query($mysql,$insert_author)) {
-					echo "Error: ".$insert_author.mysqli_error($mysql)."\n";
+					echo "Error: ".$insert_author.mysqli_error($mysql)."<br/>";
 				}
 				$insert_book_authors = "INSERT INTO BOOK_AUTHORS VALUES ('$author_id','$isbn');";
 				if (!mysqli_query($mysql,$insert_book_authors)) {
-					echo "Error: ".$insert_book_authors.mysqli_error($mysql)."\n";
+					echo "Error: ".$insert_book_authors.mysqli_error($mysql)."<br/>";
 				}	
 				$author_id++;
 			}
 		}
 	}
-	echo "Finished loading books.csv!\n";
+
 	//////////////////////////////////////////////////////////////////////
 	//////////////////////////////////////////////////////////////////////
 	$borrowers = fopen("borrowers.csv","r");
@@ -63,8 +63,18 @@
 		$phone = mysqli_real_escape_string($mysql,trim($data[8]));
 		$insert_borrower = "INSERT INTO BORROWER VALUES ('$card_id','$ssn','$bname','$address','phone');";
 		if (!mysqli_query($mysql,$insert_borrower)) {
-			echo "Error: ".$insert_borrower.mysqli_error($mysql)."\n";
+			echo "Error: ".$insert_borrower.mysqli_error($mysql)."<br/>";
 		}
 	}
-	echo "Finished loading borrowers.csv!\n";
+
+	$count_book = "SELECT COUNT(*) AS count FROM BOOK;";
+	$result = mysqli_query($mysql,$count_book);
+	$number = mysqli_fetch_array($result);
+	echo $number['count']." row(s) inserted to BOOK.<br/>";
+	
+	$count_borrower = "SELECT COUNT(*) AS count FROM BORROWER;";
+	$result = mysqli_query($mysql,$count_borrower);
+	$number = mysqli_fetch_array($result);
+	echo $number['count']." row(s) inserted to BORROWER.<br/>";
+
 ?>
--- script.js
+++ script.js
@@ -1,15 +1,61 @@
 $("document").ready(function(){
-	$("button").click(function(){
+
+	$("header li").on({
+		mouseenter: function(){
+			$(this).css({"background-color":"black","color":"white"});
+		},
+		mouseleave: function(){
+			$(this).css({"background-color":"white","color":"black"});
+		},
+		click: function(){
+			var li_class = $(this).attr('class');
+			$("div.main").each(function(){
+				if($(this).attr('id') == li_class){
+					$(this).show();
+				}else{
+					$(this).hide();
+				}
+			});
+		}
+	});
+
+	$("button[value=loadData]").click(function(){
 		loadData();
 	});
 
+	$("#booksearch button[value=search]").click(function(){
+		var content = $("#booksearch input").val();
+		$("#booksearch .message").append(content);
+		searchBook();
+	});
+
+
+
+
+
+
+	$("button[value=test]").click(function(){
+		$("#testdiv").text("success");
+	});
 });
 
 function loadData(){
 	$.ajax({
 		url:"loadData.php",
 		success: function(response){
-			$("#message").append(response);
+			$("#home .message").append(response);
+		},
+		error: function(){
+			alert("Error: books not loaded.");
+		}
+	});
+}
+
+function searchBook(){
+	$.ajax({
+		url:"searchBook.php",
+		success: function(response){
+			$("#booksearch .message").append(response);
 		},
 		error: function(){
 			alert("Error: books not loaded.");
